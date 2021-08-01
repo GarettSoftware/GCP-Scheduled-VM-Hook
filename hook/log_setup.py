02cd3e1da700102697924a8846250ce75d9a8a73
Garett MacGowan <garettsoftware@gmail.com>
2021-08-01 14:49:41 -0400

    Move certain components of log setup outside log_setup function due to bugs with the rotating logger.

--- hook/log_setup.py
+++ hook/log_setup.py
@@ -26,6 +26,20 @@ import sys
 import logging
 import logging.handlers
 
+# Set the base logger log level
+logging.getLogger().setLevel(logging.INFO)
+
+# Set the log format
+log_formatter = logging.Formatter("%(asctime)s: %(levelname)7s > %(message)s")
+
+# Create a handler for writing the logger to stderr.
+stderr_handler = logging.StreamHandler(sys.stderr)
+stderr_handler.setFormatter(log_formatter)
+
+# Create a handler for the rotating log file.
+file_handler = logging.handlers.RotatingFileHandler("hook/logs/scheduled_vm.log", maxBytes=10000000, backupCount=6)
+file_handler.setFormatter(log_formatter)
+
 
 def get_logger(name):
     """
@@ -40,19 +54,6 @@ def get_logger(name):
     :param name: __name__ attribute of logger usage file.
     :return:
     """
-    # Set the base logger log level
-    logging.getLogger().setLevel(logging.INFO)
-
-    # Set the log format
-    log_formatter = logging.Formatter("%(asctime)s: %(levelname)7s > %(message)s")
-
-    # Create a handler for writing the logger to stderr.
-    stderr_handler = logging.StreamHandler(sys.stderr)
-    stderr_handler.setFormatter(log_formatter)
-
-    # Create a handler for the rotating log file.
-    file_handler = logging.handlers.RotatingFileHandler("hook/logs/scheduled_vm.log", maxBytes=100000, backupCount=6)
-    file_handler.setFormatter(log_formatter)
 
     logger = logging.getLogger(name)
     logger.addHandler(stderr_handler)
